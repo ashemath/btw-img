9be42b9a6a48ee7d7a52d90583d51482d128889b
Bill <ashemath@gmail.com>
2024-10-13 20:17:56 -0700

    Modifying verify-deployment.sh to work without libvirt group add.

--- scripts/verify-deployment.sh
+++ scripts/verify-deployment.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
+#if [ -z $1 ] ; then
+#    echo "loading default.ini"
+#    . ./default.ini;
+#else
+#    if [ $1 = '-c' ] ; then
+#        echo "loading config from $2";
+#        . ./$2;
+#    fi
+#fi
+
+
 ## Wait for the IP address to update in libvirt's DHCP service
 ## display helpful suggestion for how to connect.
 
 PING=0
+IP="Not Found...yet..."
 echo "Waiting for connection..."
 while [ $PING = 0 ];
 do
     sleep 3;
-    IP=$(env LIBVIRT_DEFAULT_URI="qemu:///system" virsh net-dhcp-leases --network default \
-        | grep $NAME | sed "s/\ /+/g" | cut -d"+" -f16 | cut -d'/' -f1);
-    echo $IP
-    TESTPING=$(ping -c 1 $IP | grep 100%);
-    if [ "$TESTPING" != "" ] ; then
+    TESTPING=$(ping -c 1 $NAME 2> /dev/null | grep " 0%");
+    if [ -z "$TESTPING" ] ; then
         echo "..."
     else
         PING=1;
+        IP=$(host $NAME | head -n1 | cut -d" " -f4)
         echo -e "Deployment successful! try connecting with:\nssh -i $SSHKEYFILE $USER@$IP"
     fi
 done
