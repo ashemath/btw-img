c2944a42599878fda5a9af9d9425b98b18bf7669
Bill Ashe <ashew@wwu.edu>
2024-10-15 08:40:27 -0700

    Silence error on the ping test.

--- scripts/verify-deployment.sh
+++ scripts/verify-deployment.sh
@@ -19,7 +19,7 @@ do
     echo "NAME is $NAME"
     IP=$(dig $NAME @192.168.122.1 | grep ".*IN.*A.*192.168.122" | sed "s/\t//g" | sed "s/INA/+/g" | cut -d"+" -f2);
     echo "IP is set to: $IP";
-    TESTPING=$(ping -c 1 $IP | grep " 0%");
+    TESTPING=$(ping -c 1 $IP | grep " 0%" 2>/dev/null);
     if [ -z "$TESTPING" ] ; then
         echo "..."
     else
