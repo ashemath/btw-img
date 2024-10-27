df3c35ddce6e5c7c8da418175be53cbcd1903342
Bill <ashemath@gmail.com>
2024-10-26 20:34:19 -0700

    Improving password generator

--- scripts/btwpasswd.py
+++ scripts/btwpasswd.py
@@ -1,11 +1,13 @@
 #!/bin/env python3
 
+import string
 import secrets
 # On standard Linux systems, use a convenient dictionary file.
 # Other platforms may need to provide their own word-list.
 with open('/usr/share/dict/words') as f:
     words = [word.strip() for word in f]
-    password = ' '.join(secrets.choice(words) for i in range(4))
+    password = '_'.join(secrets.choice(words) for i in range(1))
+    password += ''.join(secrets.choice(string.digits) for i in range(3))
 f.close()
 
 print(password)
