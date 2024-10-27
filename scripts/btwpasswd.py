a8c111de14acf84a06f7c3bfc0402a974589f25a
Bill <ashemath@gmail.com>
2024-10-26 20:28:09 -0700

    Adding prepare-cloud-img to prep for debian build. Adding btwpasswd.py

---
+++ scripts/btwpasswd.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+import secrets
+# On standard Linux systems, use a convenient dictionary file.
+# Other platforms may need to provide their own word-list.
+with open('/usr/share/dict/words') as f:
+    words = [word.strip() for word in f]
+    password = ' '.join(secrets.choice(words) for i in range(4))
+f.close()
+
+print(password)
